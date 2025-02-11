05c72028b0e3d64d996b07fd839383a5eef5e3ca
amangeldi0 <amanimanbekov12@gmail.com>
2025-02-11 13:49:11 -0800

    sql/migrations
    - fixed migrations order deleting query
    
    - change like id type and like logic

--- sql/migrations/001_users.sql
+++ sql/migrations/001_users.sql
@@ -22,17 +22,19 @@ CREATE TABLE posts (
 );
 
 CREATE TABLE likes (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY,
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    entity_id UUID NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
-    entity_type TEXT NOT NULL,
+    entity_id UUID NOT NULL,
+    entity_type TEXT NOT NULL CHECK (entity_type IN ('post', 'comment')),
     created_at TIMESTAMP NOT NULL,
     CONSTRAINT unique_like UNIQUE (user_id, entity_id, entity_type)
 );
+
 CREATE TABLE comments (
     id UUID PRIMARY KEY,
     entity_id UUID NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
     entity_type TEXT NOT NULL,
+    is_edited boolean default false,
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
@@ -43,8 +45,8 @@ CREATE TABLE comments (
 
 -- +goose Down
 
-DROP TABLE users;
-DROP TABLE posts;
 DROP TABLE comments;
 DROP TABLE likes;
+DROP TABLE posts;
+DROP TABLE users;
 
