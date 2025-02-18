363c93d84b39ac87d1e9209e682a428477796184
amangeldi0 <amanimanbekov12@gmail.com>
2025-02-17 16:50:30 -0800

    queries/posts
    - fixed query now it returns is post liked by user.

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -13,7 +13,7 @@ SELECT
     p.created_at,
     p.updated_at,
     COALESCE(l.like_count, 0) AS like_count,
-    COALESCE(lb.liked, false) AS liked_by_user  -- SQLC теперь понимает колонку
+    COALESCE(lb.liked_by_user, false) AS liked_by_user
 FROM posts p
          LEFT JOIN (
     SELECT entity_id, COUNT(*) AS like_count
@@ -22,12 +22,11 @@ FROM posts p
     GROUP BY entity_id
 ) l ON p.id = l.entity_id
          LEFT JOIN (
-    SELECT DISTINCT ON (l.entity_id) l.entity_id, true AS liked
+    SELECT l.entity_id, true AS liked_by_user
     FROM likes l
     WHERE l.user_id = $1 AND l.entity_type = 'post'
 ) lb ON p.id = lb.entity_id
 ORDER BY p.created_at DESC;
-
 
 
 -- name: CreatePost :one
